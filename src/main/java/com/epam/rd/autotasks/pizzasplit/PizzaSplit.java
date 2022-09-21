708baf8a6671b6eadc21c1217b899db76e02c778
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-21 15:30:04 +0300

    edited main

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,9 +1,32 @@
 package com.epam.rd.autotasks.pizzasplit;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
         //Write a program, reading number of people and number of pieces per pizza and then
         //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+        Scanner scanner = new Scanner(System.in);
+        int numberPeople;
+        int numberPieces;
+        int numberPizzas = 1;
+        boolean numberFound = false;
+
+        numberPeople = scanner.nextInt();
+        numberPieces = scanner.nextInt();
+
+        while(!numberFound){
+
+            if((numberPizzas * numberPieces) % numberPeople == 0){
+                numberFound = true;
+            }
+            else{
+                numberPizzas ++;
+            }
+        }
+
+        System.out.println(numberPizzas);
+
 
 
 
